d91a594471fe76c724f0d9009d89a312848f19d1
Rafael Moura <rafaelmoura.dev@gmail.com>
2020-07-12 11:14:56 -0300

    rev

--- wiiu-client/build/dictionary.d
+++
@@ -1,4 +0,0 @@
-dictionary.o: \
- C\:/devkitPro/projects/wiiupcx/wiiu-client/src/vendor/iniparser/dictionary.c \
- C\:/devkitPro/projects/wiiupcx/wiiu-client/src/vendor/iniparser/dictionary.h
-C\:/devkitPro/projects/wiiupcx/wiiu-client/src/vendor/iniparser/dictionary.h:
--- wiiu-client/build/iniparser.d
+++
@@ -1,6 +0,0 @@
-iniparser.o: \
- C\:/devkitPro/projects/wiiupcx/wiiu-client/src/vendor/iniparser/iniparser.c \
- C\:/devkitPro/projects/wiiupcx/wiiu-client/src/vendor/iniparser/iniparser.h \
- C\:/devkitPro/projects/wiiupcx/wiiu-client/src/vendor/iniparser/dictionary.h
-C\:/devkitPro/projects/wiiupcx/wiiu-client/src/vendor/iniparser/iniparser.h:
-C\:/devkitPro/projects/wiiupcx/wiiu-client/src/vendor/iniparser/dictionary.h:
