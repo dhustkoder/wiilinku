3a6b1b5f0bdfb9a7efb6a23571701de8e3552fa4
Rafael Moura <rafaelmoura.dev@gmail.com>
2020-07-27 18:59:15 -0700

    revision win32.bat
    rev

--- win32.bat
+++ win32.bat
@@ -2,41 +2,46 @@
 set BUILD_TYPE=%1
 set RUN_AFTER_BUILD="%2"=="run"
 
-set BUILD_DIR="win32_build"
 
+set CC=cl
+
+REM CFLAGS SETUP
+set CDEFS=/D_CRT_SECURE_NO_WARNINGS /D_WINSOCK_DEPRECATED_NO_WARNINGS /DWIIUPCX_HOST
+set CDEFS_DEBUG=%CDEFS% /DDEBUG
+set CDEFS_RELEASE=%CDEFS% /DNDEBUG
 set INCLUDE_DIRS=/Isrc /Isrc\win32 /I"externals\ViGEmClient\include"
+set CFLAGS_RELEASE=/Ox /DEBUG:NONE %CDEFS_RELEASE% %INCLUDE_DIRS% 
+set CFLAGS_DEBUG=/Od /Zi /DEBUG:FULL %CDEFS_DEBUG% %INCLUDE_DIRS%  
 
+
+REM LDFLAGS SETUP
 set INCLUDE_LIBS_DEBUG=/LIBPATH:"externals\ViGEmClient\lib\debug\x64"
 set INCLUDE_LIBS_RELEASE=/LIBPATH:"externals\ViGEmClient\lib\release\x64"
-set SRC=src\\win32\*.c src\\*.c
-
 set LIBS=user32.lib gdi32.lib winmm.lib ws2_32.lib^
         setupapi.lib ViGEmClient.lib mfreadwrite.lib^
         mfplat.lib mfuuid.lib ole32.lib
+set LIBS_DEBUG=%LIBS% msvcrtd.lib vcruntimed.lib
+set LDFLAGS=/SUBSYSTEM:WINDOWS
+set LDFLAGS_DEBUG=%LDFLAGS% %INCLUDE_LIBS_DEBUG% %LIBS_DEBUG%
+set LDFLAGS_RELEASE=%LDFLAGS% %INCLUDE_LIBS_RELEASE% %LIBS%
 
-set LIBS_DEBUG=msvcrtd.lib vcruntimed.lib
 
-set CC=cl
-set CDEFS=/D_CRT_SECURE_NO_WARNINGS /D_WINSOCK_DEPRECATED_NO_WARNINGS /DWIIUPCX_HOST
-set CFLAGS_DEFAULT=%CDEFS% /wd4028 /wd4214 /wd4047 /wd4210 /wd4204 /W4  %INCLUDE_DIRS%
+REM SOURCE SETUP
+set SRC=src\\win32\*.c src\\*.c
 
-set CFLAGS_DEBUG=/Od /Zi /DEBUG:FULL /DDEBUG
-set LDFLAGS_DEBUG=%INCLUDE_LIBS_DEBUG% %LIBS% %LIBS_DEBUG% /SUBSYSTEM:WINDOWS
-
-set CFLAGS_RELEASE=/Ox /DEBUG:NONE /DNDEBUG
-set LDFLAGS_RELEASE=%INCLUDE_LIBS_RELEASE% %LIBS% /SUBSYSTEM:WINDOWS
 
 if "%BUILD_TYPE%"=="release" goto build_release else goto build_debug
 
-
 :build_debug
-set CFLAGS=%CFLAGS_DEFAULT% %CFLAGS_DEBUG%
-set LDFLAGS=%LDFLAGS_DEBUG%
+	set CFLAGS=%CFLAGS_DEBUG%
+	set LDFLAGS=%LDFLAGS_DEBUG%
+	set BUILD_DIR="win32_debug_build"
 goto compile
 
 :build_release
-set CFLAGS=%CFLAGS_DEFAULT% %CFLAGS_RELEASE%
+set CFLAGS=%CFLAGS_RELEASE%
 set LDFLAGS=%LDFLAGS_RELEASE%
+set BUILD_DIR="win32_release_build"
 goto compile
 
 :compile
