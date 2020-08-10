f33fd36699aee4c11d62a498d4f5a882724d6593
Rafael Moura <rafaelmoura.dev@gmail.com>
2020-08-10 20:39:58 -0300

    revision error.c

--- src/win32/error.c
+++ src/win32/error.c
@@ -3,27 +3,22 @@
 #include "utils.h"
 #include "error.h"
 
-#define ISSUE_REPORTING_STR_LEN (45)
+
 #define LAST_ERROR_BUFSZ (196)
 
-const char issue_reporting_str[ISSUE_REPORTING_STR_LEN + 1] = ("\nIssue report: github.com/dhustkoder/wiilinku");
-char last_error_str[LAST_ERROR_BUFSZ +  ISSUE_REPORTING_STR_LEN + 1];
+const char issue_reporting_str[] = "\nIssue report: github.com/dhustkoder/wiilinku\0";
+char last_error_str[LAST_ERROR_BUFSZ + sizeof(issue_reporting_str)];
 
 
 void set_last_error(const char* message)
 {
-	WLU_ASSERT(strlen(issue_reporting_str) == ISSUE_REPORTING_STR_LEN);
-
 	size_t len = strlen(message);
 	
-	if (len > LAST_ERROR_BUFSZ) {
+	if (len > LAST_ERROR_BUFSZ)
 		len = LAST_ERROR_BUFSZ;
-	}
 
 	memcpy(last_error_str, message, len);
-	memcpy(last_error_str + len, issue_reporting_str, ISSUE_REPORTING_STR_LEN);
-
-	last_error_str[len + ISSUE_REPORTING_STR_LEN] = '\0';
+	memcpy(last_error_str + len, issue_reporting_str, sizeof(issue_reporting_str));
 }
 
 const char* get_last_error(void)
